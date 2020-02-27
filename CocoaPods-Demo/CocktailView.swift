4f60f114f23c4bcab6d2896297e84088bbcf4499
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-02-27 12:29:03 -0500

    random drink is pulled up with ingredients

--- CocoaPods-Demo/CocktailView.swift
+++ CocoaPods-Demo/CocktailView.swift
@@ -10,22 +10,32 @@ import UIKit
 import SnapKit
 
 class CocktailView: UIView {
-
+    
     public lazy var cocktailNameLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.font = UIFont(name: "Helvetica", size: 40)
         label.numberOfLines = 2
         label.text = "Cocktail name here"
+        label.textAlignment = .center
         return label
     }()
     
     public lazy var cocktailImage: UIImageView = {
         let image = UIImageView()
         image.contentMode = .scaleAspectFit
-        image.image = UIImage(named: "people.fill")
+        image.image = UIImage(named: "mic")
         return image
     }()
- 
+    
+    public lazy var strIngridient: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Helvetica", size: 20)
+        label.numberOfLines = 0
+        label.text = "ingredients start here"
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         commonInit()
@@ -39,7 +49,31 @@ class CocktailView: UIView {
     
     
     private func commonInit(){
-        
+        setupCocktailImageConstraints()
+        setupLabel()
+        setupIngredients()
+    }
+    
+    private func setupCocktailImageConstraints() {
+        addSubview(cocktailImage)
+        cocktailImage.snp.makeConstraints { (make) in
+            make.top.leading.bottom.trailing.equalToSuperview()
+            
+        }
+    }
+    
+    private func setupLabel(){
+        addSubview(cocktailNameLabel)
+        cocktailNameLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().inset(200)
+        }
+    }
+    
+    private func setupIngredients(){
+        addSubview(strIngridient)
+        strIngridient.snp.makeConstraints { (make) in
+            make.top.equalTo(cocktailNameLabel).inset(460)
+        }
     }
 
 }
--- CocoaPods-Demo/CocktailViewController.swift
+++ CocoaPods-Demo/CocktailViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CocktailViewController: UIViewController {
+    
+    private let cocktailView = CocktailView()
+    
+    override func loadView() {
+        view = cocktailView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        fetchCocktail()
+    }
+    
+    private func fetchCocktail() {
+        APIClient.fetchCocktail { (result) in
+            switch result {
+            case .failure(let error):
+                print("oops no cocktail for you \(error)")
+            case .success(let cocktail):
+                DispatchQueue.main.async {
+                    self.updateUI(cocktail)
+
+                }
+            }
+            
+        }
+    }
+    private func updateUI(_ cocktail: Cocktail) {
+        guard let imageURL = URL(string: cocktail.strDrinkThumb) else {
+            return
+        }
+        cocktailView.cocktailImage.kf.setImage(with: imageURL)
+        cocktailView.cocktailNameLabel.text = cocktail.strDrink
+        cocktailView.strIngridient.text = "\(cocktail.strIngredient1 ?? "")\n\(cocktail.strIngredient2 ?? "")\n\(cocktail.strIngredient3 ?? "")\n\(cocktail.strIngredient4 ?? "")\n\(cocktail.strIngredient5 ?? "")\n\(cocktail.strIngredient6 ?? "")\n\(cocktail.strIngredient7 ?? "")\n\(cocktail.strIngredient8 ?? "")"
     }
 
 
